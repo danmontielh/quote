31ecb344b20144fc06e205732910adebd20b43c9
Dan Mon <Dan Mon>
2017-01-17 22:02:55 -0600

    Done!

--- js/machine.js
+++ js/machine.js
@@ -1,4 +1,21 @@
-$(".logo").click(function(){
-
-	alert("well");
+$("button").on("click", function(){
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+  .done(update);
+;
 })
+
+function update(response) {
+  var content = response.quoteText;
+  var author = response.quoteAuthor;
+  $("#quote-text").text(content);
+  $("#author").text(author);
+};
+
+/*$("#quote-text").text(content);
+$("#author").text(aut);*/
+
+
+
+
+
+
